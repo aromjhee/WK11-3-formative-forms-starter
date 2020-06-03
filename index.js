93e1e5361134e5568d3d969c873a981bda62e5e1
Arom Jhee <aromjhee@gmail.com>
2020-06-03 12:43:34 -0700

    require urlencoded, csurf library and include validations for form input fields and to display them if there are errors

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const csrfProtection = csrf({ cookie: true });
 
 app.set("view engine", "pug");
 app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
 
 const users = [
   {
@@ -27,6 +28,40 @@ app.get("/create", csrfProtection, (req, res) => {
     title: "Create a user", csrfToken: req.csrfToken()
   });
 });
+
+const validateFields = (req, res, next) => {
+  const errors = [];
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+
+  if (!firstName) errors.push("Please provide a first name.")
+  if (!lastName) errors.push("Please provide a last name.")
+  if (!email) errors.push("Please provide an email.")
+  if (!password) errors.push("Please provide a password.")
+  if (password !== confirmedPassword) errors.push("The provided values for the password and password confirmation fields did not match.")
+
+  req.errors = errors;
+  next();
+}
+
+app.post("/create", csrfProtection, validateFields, (req, res) => {
+  const { firstName, lastName, email, password, confirmedPassword } = req.body;
+  const errors = req.errors;
+  
+  if (errors.length > 0) {
+    res.render("create", {
+      title: "Create a user",
+      errors,
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmedPassword
+    });
+    return;
+  }
+
+  res.redirect("/");
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
